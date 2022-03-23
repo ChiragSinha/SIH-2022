175d7429437ff9846599e3a146d5ac05e439981f
ChiragSinha <chirag.sinha@scaler.com>
2022-03-24 00:10:55 +0530

    Added initial setup

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const PORT = 7000
+const {MONGOURI} = require('./keys') // add mongo auth keys in keys.js folder
+
+mongoose.connect(MONGOURI)
+
+mongoose.connection.on('connected', () => {
+    console.log("Connected to mongodb")
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log("error", err)
+})
 
 
 app.get('/', (req, res) => {
